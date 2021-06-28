1236ce313e2f146fb9c3798664f214c80b987b46
alaahahi <alaa.hahe@gmail.com>
2021-06-28 11:21:42 +0300

    fix

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -108,7 +108,7 @@
             <label for="phone" class="col-form-label">{{ __('voyager::generic.card_type') }}:</label>
             <input type="text" class="form-control"  id="card_type_id" disabled>
             <span id="card_type">
-            <select class="form-control select2-ajax select2-hidden-accessible " name="card_type_id" data-get-items-route="http://localhost/cms/public/admin/cards/relation" data-get-items-field="card_belongsto_card_type_relationship_1" data-method="add" data-select2-id="1" tabindex="-1" aria-hidden="true">
+            <select class="form-control select2-ajax select2-hidden-accessible " name="card_type_id" data-get-items-route="https://savingservices.net/cms/public/admin/cards/relation" data-get-items-field="card_belongsto_card_type_relationship_1" data-method="add" data-select2-id="1" tabindex="-1" aria-hidden="true">
                     <option value="" data-select2-id="3">None</option>
             </select>
             </span>
